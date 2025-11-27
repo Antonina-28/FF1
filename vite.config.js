a7e540b4390d18bc033b945146891c9111fc937b
Antonina Sorokan <atretyakova28@yandex.ru>
2025-11-27 14:50:11 +0300

    Adds file vite.config.js and new form for calculator

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        // Для JavaScript файлов
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        
+        // Для остальных ресурсов (изображения, шрифты, CSS)
+        assetFileNames: (assetInfo) => {
+          const extType = assetInfo.name.split('.').pop();
+          
+          if (/png|jpe?g|jfif|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return 'images/[name]-[hash][extname]';
+          }
+          
+          if (/woff|woff2|eot|ttf|otf/i.test(extType)) {
+            return 'fonts/[name]-[hash][extname]';
+          }
+          
+          if (/css/i.test(extType)) {
+            return 'css/[name]-[hash][extname]';
+          }
+          
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
